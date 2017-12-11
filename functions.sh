6143ad5b39792e03de33c414f56543d261ea4da9
xspirus <spirosdontas@gmail.com>
2017-12-12 00:38:01 +0200

    Added various echo functions with different colors

--- functions.sh
+++ functions.sh
@@ -1,8 +1,48 @@
-#!/bin/zsh
+#!/bin/sh
 
-# A common function
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+	ncolors=$(tput colors)
+fi
 
-fancy_echo() {
-	local fmt="$1"; shift
-	printf "\n$fmt\n\n" "$@"
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+	ERROR="$(tput setaf 1)"
+	DONE="$(tput setaf 2)"
+	WARNING="$(tput setaf 3)"
+	INFO="$(tput setaf 4)"
+	BOLD="$(tput bold)"
+	NORMAL="$(tput sgr0)"
+else
+	ERROR=""
+	DONE=""
+	WARNING=""
+	INFO=""
+	BOLD=""
+	NORMAL=""
+fi
+
+echo_error() {
+	echo -e "${ERROR}$@${NORMAL}"
+}
+
+echo_done() {
+	echo -e "${DONE}$@${NORMAL}"
+}
+
+echo_warning() {
+	echo -e "${WARNING}$@${NORMAL}"
+}
+
+echo_info() {
+	echo -e "${INFO}$@${NORMAL}"
+}
+
+install() {
+	sudo pacman -Sy --needed --noconfirm $@
+	if [ $? -ne 0 ]; then
+		echo_error "Pacman had an error while installing the packages"
+		exit 1
+	else
+		echo_done "Pacman finished its job"
 }
