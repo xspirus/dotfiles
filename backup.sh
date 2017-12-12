06b2e01dc4c9150285b5c264acb58e0be4c42b50
xspirus <spirosdontas@gmail.com>
2017-12-12 19:57:54 +0200

    Removed sublime packages. Sublime does not make the config files

--- backup.sh
+++ backup.sh
@@ -7,14 +7,6 @@ dotfiles=(
 	tmux.conf
 )
 
-sublime=(
-	"Preferences.sublime-settings"
-	"Package Control.sublime-settings"
-	"Monokai.tmTheme"
-)
-
-sublimedir=~/.config/sublime-text-3/Packages/User
-
 today=`date +%Y%m%d`
 
 # Make backup of old
@@ -35,27 +27,5 @@ echo_info "Symlinking configuration files from github"
 for file in ${dotfiles[@]}
 do
 	ln -sv ~/dotfiles/.$file ~/.$file
-	source ~/.$file
-done
-echo_done "Symlink finished"
-
-# Backup sublime settings etc
-
-echo
-echo_info "Making backup of local sublime files"
-for file in ${sublime[@]}
-do
-	[ -e $sublimedir/$file ] && [ ! -L $sublimedir/$file ] && mv -v $sublimedir/$file $sublimedir/$file.bak.$today
-	[ -L $sublimedir/$file ] && unlink -v $sublimedir/$file
-done
-echo_done "Backup finished"
-
-# Symlink sublime files
-
-echo
-echo_info "Symlinking sublime files"
-for file in ${sublime[@]}
-do
-	ln -sv ~/dotfiles/$file $sublimedir/$file
 done
 echo_done "Symlink finished"
